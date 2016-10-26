de348886ba60446b80780ac60dc4d8b3ebd94880
joel <joel@localhost.localdomain>
2016-10-26 04:31:58 +0300

    making use of the if elif and else statements

--- controlflow.py
+++ controlflow.py
@@ -6,3 +6,14 @@ i = 1
 while i <= 10:
     print i
     i +=1
+
+#if,elif and else statements
+
+a = 70
+b = 80
+if a > b :
+    print "hello"
+elif a == 10:
+    print "hohoho"
+else:
+    print "welcome"
